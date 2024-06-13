4d95335747fa3408b554b69c4514551a353bcf85
venky180 <venky.180@gmail.com>
2024-06-13 09:40:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def main():
             
         ],
         }
-    st_echarts(options=options, height="600px",width="1000px")
+    st_echarts(options=options, height="600px",width="1500px")
 
 
        
